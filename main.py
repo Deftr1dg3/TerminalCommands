45a5e02b5aa6ce8405bfcb8aaaa76fa4c2aeeab7
Deftr1dg3 <inem613@gmail.com>
2022-12-07 12:28:00 +0200

    commit 12:28:00 2022-12-07

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import re
 
 def validate_password_strenght(password: str) -> bool:
+    '''Function takes string as an argument, delets possible
+    spases at the begining and in the end, checks if the string
+    contains required charcters and returns "bool" value'''
+
     p = password.strip()
     return bool(re.match(r'^(?=.*[0-9])'\
                         r'(?=(.*[a-z]){2,})'\
                         r'(?=(.*[A-Z]){2,})'\
                         r'(?=(.*[!@#$%^&*)(?.><;:\"]))'\
                         r'.{8,32}$', p))
-
 
 
 
